4aea91d412cf7c1c8b227d774b2391cdf49bb55e
AnnapoornaK <annapoorna.kamath@codeandtheory.com>
2026-07-23 16:45:20 +0530

    Passing data between screens

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import { MEALS } from "../data/dummy-data";
 
-function MealsOverviewScreen() {
+function MealsOverviewScreen({ route }) {
+const category = route.params.categoryId;
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview screen</Text>
+      <Text>Meals Overview screen - {category}</Text>
     </View>
   );
 }
